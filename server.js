9c1022bcb3d7fc7ccbab6d8cb94cbe472367ae02
VedhanshB <vedhansh2011@icloud.com>
2022-02-10 20:06:56 +0530

    lss

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// create a api 
-app.get('/api', (req, res) => {
-    res.json({
-        name: 'John',
-        age: 30
-    });
-});
-
-//make a 404 error
-app.get('*', (req, res) => {
-    res.sendStatus(404);
-});
-
-
 console.log('Server running on port 9090');
 
 app.listen(9090);
